17d4a8ac82236bfcb92b3528bbd1188f28b3df77
Roberts Ivanovs <robertsivanovs1999@gmail.com>
2024-03-05 11:45:21 +0200

    batman

---
+++ src/main.rs
@@ -0,0 +1,54 @@
+use tokio::task::JoinSet;
+
+#[tokio::main]
+async fn main() {
+    println!("Hello, world!");
+
+    let mut set = JoinSet::new();
+    set.spawn(run1());
+    set.spawn(run2());
+
+    while let Some(task_return) = set.join_next().await {
+        println!("ERROR - task panicked! {task_return:?}");
+        set.abort_all();
+    }
+
+    // Sleep for 10 seconds to allow the spawned tasks to complete
+    tokio::time::sleep(std::time::Duration::from_secs(10)).await;
+}
+
+async fn run1() {
+    match tokio::spawn(async {
+        // log timestamp every 1 sec
+        loop {
+            tokio::time::sleep(std::time::Duration::from_secs(1)).await;
+            println!("timestamp: {}", chrono::Utc::now());
+        }
+    })
+    .await
+    {
+        Ok(succsess) => {
+            println!("success: {:?}", succsess);
+        }
+        Err(err) => {
+            println!("error: {:?}", err);
+        }
+    }
+}
+async fn run2() {
+    match tokio::spawn(async {
+        // sleep for 3 sec then panic
+        tokio::time::sleep(std::time::Duration::from_secs(3)).await;
+
+        panic!("OMG")
+    })
+    .await
+    {
+        Ok(succsess) => {
+            println!("success: {:?}", succsess);
+        }
+        Err(err) => {
+            println!("error: {:?}", err);
+        }
+    }
+}
