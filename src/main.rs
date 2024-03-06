44d743dbc09874cc57382beff56bf066ccf4142b
Roberts Ivanovs <robertsivanovs1999@gmail.com>
2024-03-06 13:33:10 +0200

    proper usage of join sets

--- src/main.rs
+++ src/main.rs
@@ -5,52 +5,38 @@ async fn main() {
     println!("Hello, world!");
 
     let mut set = JoinSet::new();
-    let h1 = run1();
-    let h2 = run2();
-    let abort_handles = [h1.abort_handle(), h2.abort_handle()];
-    set.spawn(h1);
-    set.spawn(h2);
+    // set.spawn already spawns a tokio::task under the hood!
+    set.spawn(run1());
+    set.spawn(run2());
 
+    // On first error we shut down all tasks
     while let Some(task_return) = set.join_next().await {
+        // Common task top-level logging
         match task_return {
-            Ok(res) => match res {
-                Ok(succsess) => {
-                    println!("success: {:?}", succsess);
-                }
-                Err(err) => {
-                    println!("error: {:?}", err);
-                }
-            },
+            Ok(res) => {
+                println!("success: {:?}", res);
+            }
             Err(res) => {
-                println!("set-level join error: {:?}", res);
+                println!("join error: {:?}", res);
             }
         }
         set.abort_all();
     }
 
-    for (idx, handle) in abort_handles.iter().enumerate() {
-        println!("aborting task {idx:}");
-        handle.abort();
-    }
-
-    // As you can see the run1() task just keeps on printing without an intention of stopping 😲
+    // As you can see all tasks have been stopped! 😲
     tokio::time::sleep(std::time::Duration::from_secs(10)).await;
 }
 
-fn run1() -> JoinHandle<()> {
-    tokio::spawn(async {
-        // log timestamp every 1 sec
-        loop {
-            tokio::time::sleep(std::time::Duration::from_secs(1)).await;
-            println!("timestamp: {}", chrono::Utc::now());
-        }
-    })
+async fn run1() {
+    // log timestamp every 1 sec
+    loop {
+        tokio::time::sleep(std::time::Duration::from_secs(1)).await;
+        println!("timestamp: {}", chrono::Utc::now());
+    }
 }
-fn run2() -> JoinHandle<()> {
-    tokio::spawn(async {
-        // sleep for 3 sec then panic
-        tokio::time::sleep(std::time::Duration::from_secs(3)).await;
+async fn run2() {
+    // sleep for 3 sec then panic
+    tokio::time::sleep(std::time::Duration::from_secs(3)).await;
 
-        panic!("OMG")
-    })
+    panic!("OMG")
 }
